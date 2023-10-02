f999f006f63a852c5d8d6f180a5eebe6790e883f
Josep Maria Blasco <jose.maria.blasco@gmail.com>
2023-10-02 17:44:29 +0200

    Delete gendocs.cmd

--- ARB/standards/work-in-progress/unicode/UnicodeTools/gendocs.cmd
+++
@@ -1,4 +0,0 @@
-@Rem These work for me -- JMB
-@set ooRexxDoc=C:\ooRexxDoc-2.2.0-GA\oorexxdoc-2.2.0-GA.jar
-@set IDEAlib=C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.2\lib\*
-java -cp "%ooRexxDoc%;%IDEAlib%" org.oorexx.doc.Application ./doc/Unicode.xml
